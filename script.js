1bae770001211f8ddd6083bfb6d2ec7cd6b53066
Moodybleu <meganbenn27@icloud.com>
2022-08-23 12:17:30 -0700

    adds more javascript

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 let humanSequence = [];
+let order = [];
 let levelStart = 0 
-let intervalID = [];
+let intervalID;
 let flash;
 let compTurn;
-let noise = true
+let noise = true;
+let on = true;
 let win;
 
 
 const sequence = ['Red', 'Green', 'Blue', 'Purple'];
+const onButton = document.querySelector('#on')
 const startButton = document.querySelector('#GameStart')
 const Red = document.querySelector('#Red')
 const Blue = document.querySelector('#Blue')
@@ -15,28 +18,34 @@ const Green = document.querySelector('#Green')
 const Purple = document.querySelector('#Purple')
 const turnCounter = document.querySelector('#turn')
 
-startButton.addEventListener('click', (event) => {
-  if (startButton.clicked === true) {
+onButton.addEventListener('click', (event) => {
+  if (onButton.checked === true) {
     on = true;
-    turnCounter.innerHTML = '-';
+    turnCounter.innerHTML = "-";
   } else {
     on = false;
-    turnCounter.innerHTML = '';
-    clearColor(); 
-    clearInterval();
+    turnCounter.innerHTML = "";
+    clearColor();
+    clearInterval(intervalId);
   }
-})
+});
+
+startButton.addEventListener('click', (event) => {
+  if (on || win) {
+    playGame();
+  }
+});
 
 function playGame() {
   win = false;
   order = [];
-  playerOrder = [];
+  humanSequence = [];
   flash = 0;
   intervalID = 0;
   turn = 1;
   turnCounter.innerHTML = 1;
   good = true;
-  for (var i = 0; i < 20; i++) {
+  for (let i = 0; i < 10; i++) {
     order.push(math.floor(math.random() * 4) + 1);
   }
   compTurn = true;
@@ -69,40 +78,102 @@ function gameTurn() {
 
 function one() {
   if (noise) {
-    let audio = document.getElementById("clip1");
+    let audio = document.getElementById("RedSound");
     audio.play();
   }
   noise = true;
-  Red.style.backgroundColor = "tomato";
+  Red.style.backgroundColor = "#ff0000";
 }
 
 function two() {
   if (noise) {
-    let audio = document.getElementById("clip2");
+    let audio = document.getElementById("GreenSound");
     audio.play();
   }
   noise = true;
-  Green.style.backgroundColor = "lightgreen";
+  Green.style.backgroundColor = "#03d931";
 }
 
 function three() {
   if (noise) {
-    let audio = document.getElementById("clip3");
+    let audio = document.getElementById("BlueSound");
     audio.play();
   }
   noise = true;
-  Blue.style.backgroundColor = "lightblue";
+  Blue.style.backgroundColor = "#1f36ff";
 }
 
 function four() {
   if (noise) {
-    let audio = document.getElementById("clip4");
+    let audio = document.getElementById("PurpleSound");
     audio.play();
   }
   noise = true;
-  Purple.style.backgroundColor = "lightpurple";
+  Purple.style.backgroundColor = "CD14EB";
 }
 
+function clearColor() {
+  Red.style.backgroundColor = "darkred"
+  Green.style.backgroundColor = "darkgreen"
+  Blue.style.backgroundColor = "darkblue"
+  Purple.style.backgroundColor = "darkpurple"
+}
+
+function flashColor() {
+Red.style.backgroundColor = "";
+Green.style.backgroundColor = "";
+Blue.style.backgroundColor = "";
+Purple.style.backgroundColor = "";
+}
+
+Red.addEventListener('click', (event) => {
+  if (on) {
+    playerOrder.push(1);
+    check();
+    one();
+    if(!win) {
+      setTimeout(() => {
+        clearColor();
+      }, 300);
+    }
+  }
+})
+Green.addEventListener('click', (event) => {
+  if (on) {
+    playerOrder.push(2);
+    check();
+    two();
+    if(!win) {
+      setTimeout(() => {
+        clearColor();
+      }, 300);
+    }
+  }
+})
+Blue.addEventListener('click', (event) => {
+  if (on) {
+    playerOrder.push(3);
+    check();
+    three();
+    if(!win) {
+      setTimeout(() => {
+        clearColor();
+      }, 300);
+    }
+  }
+})
+Purple.addEventListener('click', (event) => {
+  if (on) {
+    playerOrder.push(4);
+    check();
+    four();
+    if(!win) {
+      setTimeout(() => {
+        clearColor();
+      }, 300);
+    }
+  }
+})
 
 // Start game button ✅
 // Generate random sequence of four item array ✅
