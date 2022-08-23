3d67848e72622e7d70d9e485c42a465be5fe7a8a
Moodybleu <meganbenn27@icloud.com>
2022-08-23 12:39:39 -0700

    Finishes javascript

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ function playGame() {
   turnCounter.innerHTML = 1;
   good = true;
   for (let i = 0; i < 10; i++) {
-    order.push(math.floor(math.random() * 4) + 1);
+    order.push(Math.floor(Math.random() * 4) + 1);
   }
   compTurn = true;
 
@@ -128,7 +128,7 @@ Purple.style.backgroundColor = "";
 
 Red.addEventListener('click', (event) => {
   if (on) {
-    playerOrder.push(1);
+    humanSequence.push(1);
     check();
     one();
     if(!win) {
@@ -140,7 +140,7 @@ Red.addEventListener('click', (event) => {
 })
 Green.addEventListener('click', (event) => {
   if (on) {
-    playerOrder.push(2);
+    humanSequence.push(2);
     check();
     two();
     if(!win) {
@@ -152,7 +152,7 @@ Green.addEventListener('click', (event) => {
 })
 Blue.addEventListener('click', (event) => {
   if (on) {
-    playerOrder.push(3);
+    humanSequence.push(3);
     check();
     three();
     if(!win) {
@@ -164,7 +164,7 @@ Blue.addEventListener('click', (event) => {
 })
 Purple.addEventListener('click', (event) => {
   if (on) {
-    playerOrder.push(4);
+    humanSequence.push(4);
     check();
     four();
     if(!win) {
@@ -175,11 +175,58 @@ Purple.addEventListener('click', (event) => {
   }
 })
 
+function check() {
+  if (humanSequence[humanSequence.length - 1] !== order[humanSequence.length - 1])
+    good = false;
+
+  if (humanSequence.length == 5 && good) {
+    winGame();
+  }
+
+  if (good == false) {
+    flashColor();
+    turnCounter.innerHTML = "NO!";
+    setTimeout(() => {
+      turnCounter.innerHTML = turn;
+      clearColor();
+
+      if (strict) {
+        play();
+      } else {
+        compTurn = true;
+        flash = 0;
+        humanSequence = [];
+        good = true;
+        intervalId = setInterval(gameTurn, 800);
+      }
+    }, 800);
+
+    noise = false;
+  }
+
+  if (turn == humanSequence.length && good && !win) {
+    turn++;
+    humanSequence = [];
+    compTurn = true;
+    flash = 0;
+    turnCounter.innerHTML = turn;
+    intervalId = setInterval(gameTurn, 800);
+  }
+
+}
+
+function winGame() {
+  flashColor();
+  turnCounter.innerHTML = "WIN!";
+  on = false;
+  win = true;
+}
+
 // Start game button ✅
 // Generate random sequence of four item array ✅
 // Timeout function inside "forEach" ✅
 // Display text for player -- 'Your turn!'
-// Start player turn
-// Make colors clickable after computer turn
+// Start player turn ✅
+// Make colors clickable after computer turn ✅
 // Allow only four clicks for first round and record into array 
 // Use a boolean to compare the two arrays 
